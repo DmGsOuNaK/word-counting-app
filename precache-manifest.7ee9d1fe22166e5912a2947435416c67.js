2e16de6c0dd51c6be3d9f81f0989e0efb43a7f63
DmGsOuNaK <sounak.swamiji@gmail.com>
2023-06-15 00:49:01 +0530

    Updates

---
+++ precache-manifest.7ee9d1fe22166e5912a2947435416c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17b4bec879dc0a520963",
+    "url": "/word-counting-app/static/css/main.c6f07899.chunk.css"
+  },
+  {
+    "revision": "17b4bec879dc0a520963",
+    "url": "/word-counting-app/static/js/main.17b4bec8.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/word-counting-app/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/word-counting-app/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "9abe96fca3954c7f57b1",
+    "url": "/word-counting-app/static/js/runtime~main.9abe96fc.js"
+  },
+  {
+    "revision": "1be43bc7a7df66d2cb62bd781f510f36",
+    "url": "/word-counting-app/index.html"
+  }
+];
